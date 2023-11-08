54bf12cdbe6e37d2a2c116a1bc58df57e8c937d7
Hairo Barnard <Hairo.barnard@gmail.com>
2023-11-08 13:48:09 -0500

    Completed Mock CHallenge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 // your code here
+
+const cakeAPI = "http://localhost:3000/cakes"
+const yourReview = document.getElementById('review')
+const button = document.querySelector('.button')
+const img1URL = "https://curriculum-content.s3.amazonaws.com/phase-1/phase-1-code-challenge-cake-off/let-it-drip-cake.jpg"
+let img = document.getElementById('cake-image')
+
+//recieve cake data 
+
+
+fetch(cakeAPI) 
+.then(res => res.json)
+.then ((data) => {
+    console.log(img1URL)
+});
+
+function renderImage (img){ 
+    img.forEach((img1)=> { 
+            return cakeAPI.appendChild(img1URL)
+    });
+}
+
+
